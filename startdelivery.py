4f6dabb7f8722223809dd69f3d74f7389b29f817
lestertj <lester.tan@u.nus.edu>
2018-03-30 16:06:01 +0800

    Added confirmation for start delivery

--- startdelivery.py
+++ startdelivery.py
@@ -16,17 +16,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-ORDERING_FROM, ORDER_CLOSE, ARRIVAL_TIME, PICK_UP_POINT = range(4)
+ORDERING_FROM, ORDER_CLOSE, ARRIVAL_TIME, PICK_UP_POINT, CONFIRMATION = range(5)
+
 
 def start_delivery(bot, update, chat_data):
 	chat_data[update.message.from_user.id] = {
 		'chat': update.message.chat_id,
-		'user': update.message.from_user.id
+		'user': update.message.from_user.id,
 	}
+	chat_data['confirmation'] =  False
 
 	bot.send_message(update.message.from_user.id, 'Hi! Where are you ordering food from? Send /cancel to cancel this request anytime')
 
 	return ORDERING_FROM
+
 
 def ordering_from(bot, update, chat_data):
 	user = update.message.from_user
@@ -34,10 +37,15 @@ def ordering_from(bot, update, chat_data):
 	chat_data[user.id]['location'] = update.message.text
 
 	logger.info("%s Ordering from: %s", user.first_name, update.message.text)
+
+	if chat_data['confirmation']:
+		return send_confirmation(bot, update, chat_data)
+
 	update.message.reply_text(
 		'What time will the order close? (Use HHMM, e.g. 1630 for 4:30pm, 0430 for 4:30am)')
 
 	return ORDER_CLOSE
+
 
 def order_close(bot, update, chat_data):
 	user = update.message.from_user
@@ -50,10 +58,15 @@ def order_close(bot, update, chat_data):
 		return ORDER_CLOSE
 
 	logger.info("%s Order closes: %s", user.first_name, update.message.text)
+
+	if chat_data['confirmation']:
+		return send_confirmation(bot, update, chat_data)
+
 	update.message.reply_text(
 		'What time will the order arrive? (Use HHMM, e.g. 1630 for 4:30pm, 0430 for 4:30am)')
 
 	return ARRIVAL_TIME
+
 
 def arrival_time(bot, update, chat_data):
 	user = update.message.from_user
@@ -66,16 +79,26 @@ def arrival_time(bot, update, chat_data):
 		return ARRIVAL_TIME
 
 	logger.info("%s Arrival time: %s", user.first_name, update.message.text)
+
+	if chat_data['confirmation']:
+		return send_confirmation(bot, update, chat_data)
+
 	update.message.reply_text(
 		'Where is the pickup point?')
 
 	return PICK_UP_POINT
 
-def pick_up_point(bot, update, chat_data, job_queue):
+
+def pick_up_point(bot, update, chat_data):
 	user = update.message.from_user
 
 	chat_data[user.id]['pickup'] = update.message.text
-	delivery = chat_data.pop(user.id)
+
+	return send_confirmation(bot, update, chat_data)
+
+
+def register_delivery(bot, update, delivery, job_queue):
+	user = update.message.from_user
 
 	deliveryId = database.start_delivery(delivery)
 	delivery['id'] = deliveryId
@@ -88,26 +111,59 @@ def pick_up_point(bot, update, chat_data, job_queue):
 	bot.send_message(
 		delivery['chat'],
 		ORDER_CONFIRM_MESSAGE %
-		(delivery['location'], user.first_name, delivery['closes'], delivery['arrival'], delivery['pickup'], deliveryId)
+		(delivery['location'], user.first_name, delivery['closes'], delivery['arrival'], delivery['pickup'], delivery['id'])
 	)
 
 	job1 = job_queue.run_once(
 		notifications.notify_close,
 		delivery['closes'].astimezone().replace(tzinfo=None),
 		context=delivery,
-		name= str(deliveryId) + '_close_job')
+		name=str(delivery['id']) + '_close_job')
 	job2 = job_queue.run_once(
 		notifications.notify_arrival_soon,
-		delivery['arrival'].astimezone().replace(tzinfo=None) - timedelta(minutes=int(os.environ.get('ARRIVAL_ADVANCE_NOTIFICATION_TIME', '15'))),
+		delivery['arrival'].astimezone().replace(tzinfo=None) - timedelta(
+			minutes=int(os.environ.get('ARRIVAL_ADVANCE_NOTIFICATION_TIME', '15'))),
 		context=delivery)
 	job3 = job_queue.run_once(
 		notifications.notify_arrival,
 		delivery['arrival'].astimezone().replace(tzinfo=None),
 		context=delivery)
 
-	logger.info('%s\'s order: %s', user.first_name, delivery)
-
+	logger.info('%s\'s delivery: %s', user.first_name, delivery)
 	return ConversationHandler.END
+
+
+def edit_choice(bot, update, chat_data, job_queue):
+	choice = update.message.text
+	if choice == '/yes':
+		return register_delivery(bot, update, chat_data, job_queue)
+	elif choice == '/from':
+		update.message.reply_text("Please enter the new location")
+		return ordering_from(bot, update, chat_data)
+	elif choice == '/closing':
+		update.message.reply_text("Please enter the new closing time")
+		return order_close(bot, update, chat_data)
+	elif choice == '/arriving':
+		update.message.reply_text("Please enter the new arrival time")
+		return arrival_time(bot, update, chat_data)
+	elif choice == '/pickup':
+		update.message.reply_text("Please enter the new pickup location")
+		return pick_up_point(bot, update, chat_data)
+	else:
+		update.message.reply_text("Invalid command, please try again")
+		return CONFIRMATION
+
+def send_confirmation(bot, update, chat_data):
+	chat_data['confirmation'] = True
+	userid = update.message.from_user.id
+	text = "Reply /yes to confirm your details, or click on the headers to edit them:\n" \
+		   "/from: " + chat_data[userid]['location'] + "\n" \
+		   "/closing: " + chat_data[userid]['closes'] + "\n" \
+		   "/arriving: " + chat_data[userid]['arrival'] + "\n" \
+		   "/pickup: " + chat_data[userid]['pickup']
+	update.message.reply_text(text)
+	return CONFIRMATION
+
 
 def cancel(bot, update):
 	user = update.message.from_user
@@ -115,6 +171,7 @@ def cancel(bot, update):
 	update.message.reply_text(
 		"Your request to start a delivery has been cancelled!")
 	return ConversationHandler.END
+
 
 def datetime_from_text(text):
 	r_date = date.today().strftime(r" %Y %m %d +0800")
@@ -136,8 +193,14 @@ start_delivery_conv_handler = ConversationHandler(
 		ARRIVAL_TIME: [MessageHandler(Filters.text, arrival_time, pass_chat_data=True),
 					   CommandHandler('cancel', cancel)],
 
-		PICK_UP_POINT: [MessageHandler(Filters.text, pick_up_point, pass_job_queue=True, pass_chat_data=True),
-						CommandHandler('cancel', cancel)]
+		PICK_UP_POINT: [MessageHandler(Filters.text, pick_up_point, pass_chat_data=True),
+						CommandHandler('cancel', cancel)],
+
+		CONFIRMATION: [CommandHandler('yes', edit_choice, pass_job_queue=True, pass_chat_data=True),
+					   CommandHandler('pickup', edit_choice, pass_job_queue=True, pass_chat_data=True),
+					   CommandHandler('arriving', edit_choice, pass_job_queue=True, pass_chat_data=True),
+					   CommandHandler('closing', edit_choice, pass_job_queue=True, pass_chat_data=True),
+					   CommandHandler('from', edit_choice, pass_job_queue=True, pass_chat_data=True)]
 	},
 	fallbacks=[CommandHandler('cancel', cancel)]
 )
