dfe6a035118841260412a69202ce03c8c8080b55
Casper <personalvolition@hotmail.com>
2018-04-09 10:17:10 +0800

    Update startdelivery.py

--- startdelivery.py
+++ startdelivery.py
@@ -12,7 +12,7 @@ import os
 from datetime import date, datetime, timedelta
 
 ORDER_CONFIRM_MESSAGE = 'Delivery for {dish} from {location} by {username} {usertag}\n' \
-						'Price: {price} (+{markup} delivery fee)\n' \
+						'Price: ${price} (+${markup} delivery fee)\n' \
 						'Closing: {closes}\n' \
 						'Arriving: {arrival}\n' \
 						'Pickup: {pickup}\n' \
