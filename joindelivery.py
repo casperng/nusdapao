468a37b558f49e47c65e28e28ba755081205bb62
Casper <personalvolition@hotmail.com>
2018-04-10 13:27:12 +0800

    Updated item count message

--- joindelivery.py
+++ joindelivery.py
@@ -44,7 +44,7 @@ def delivery_id(bot, update, user_data):
 
 	logger.info("%s for delivery: %s", user.first_name, update.message.text)
 	update.message.reply_text(
-		'How many of this item are you getting?')
+		'How many of this item are you getting? (Please enter a numerical figure, additional comments can be filled in under remarks)')
 
 	user_data['deliveryId'] = update.message.text
 
